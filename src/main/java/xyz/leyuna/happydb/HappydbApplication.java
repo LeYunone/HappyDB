dd5ffc936158fb5c5c0e159e20b651a3feaf8eda
leyunone <365627310@qq.com>
2022-05-19 22:24:57 +0800

    Initial commit

---
+++ src/main/java/xyz/leyuna/happydb/HappydbApplication.java
@@ -0,0 +1,13 @@
+package xyz.leyuna.happydb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappydbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappydbApplication.class, args);
+    }
+
+}
